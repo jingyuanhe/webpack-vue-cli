471a5a36f9400d0be15ef3ad36d5839cf0ac0efc
e01922059 <01922059@yto.net.cn>
2020-04-24 18:13:02 +0800

    vconsole

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import store from './store';
-// 移动端调试
-import VConsole from 'vconsole';
-// eslint-disable-next-line no-unused-vars
-const vConsole = new VConsole();
+if (process.env.NODE_ENV === 'development') {
+  const VConsole = require('vconsole');
+  // eslint-disable-next-line no-unused-vars
+  const vConsole = new VConsole();
+}
 Vue.directive('wechat-title', {
   inserted (el, bind) {
     document.title = bind.value;
